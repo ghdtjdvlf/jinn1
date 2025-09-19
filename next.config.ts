005a5a81a711a9cec1acb30a2f4114d1e94a9790
ghdtjdvlf <tjdvlf0416@gmail.com>
2025-09-19 15:08:14 +0900

    이미지 최적화

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,14 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  devIndicators: false, // 개발자용 빌드 인디케이터 제거
-    eslint: {
-    ignoreDuringBuilds: true, // ✅ 빌드 시 ESLint 오류 무시
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com", // user-attachments, ?raw=1
-      },
-      {
-        protocol: "https",
-        hostname: "user-images.githubusercontent.com",
-      },
+      { protocol: "https", hostname: "raw.githubusercontent.com" },
+      { protocol: "https", hostname: "github.com" },
+      { protocol: "https", hostname: "user-images.githubusercontent.com" },
+      { protocol: "https", hostname: "i.ibb.co" },
     ],
-    unoptimized: true, // ✅ Netlify 정적 export 시 필수
   },
-  output: "export", // ✅ Netlify 배포용 (정적 export)
+  output: "export",
 };
-
-export default nextConfig;
